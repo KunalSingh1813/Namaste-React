72a79e19f7add425e527b50c6dcab28d1d7211fc
Kunal Singh <singh.kunal1813@gmail.com>
2023-04-18 12:48:40 +0530

    chapter3

--- App.js
+++ App.js
@@ -1,50 +1,53 @@
 /*Parcel does all these below mentioned things
-*
-*Created A server
-*HMR  - Hot Module Replacement
-*File Watcher algorithm -> C++
-*Bundling
-*Minify
-*Cleaning our code
-*Dev and production build
-*superfast build algorithm
-*image optimization also
-*caching while development
-*Compatible with older version of browser
-*HTTPS on dev
-*port number
-*consistent hashing algorithm
-*
-*
-*Transitive Dependencies
-*/
+ *
+ *Created A server
+ *HMR  - Hot Module Replacement
+ *File Watcher algorithm -> C++
+ *Bundling
+ *Minify
+ *Cleaning our code
+ *Dev and production build
+ *superfast build algorithm
+ *image optimization also
+ *caching while development
+ *Compatible with older version of browser
+ *HTTPS on dev
+ *port number
+ *consistent hashing algorithm
+ *Tree Shaking -- Removing unwanted code
+ *
+ *Transitive Dependencies
+ */
 // Create nested React Elements
 import React from "react";
-import  ReactDOM  from "react-dom/client";
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "Heading 1 from parcel"
+import ReactDOM from "react-dom/client";
+
+//JSX => React.createElement => Object => HTML(DOM)
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
+
+//React Component
+//Functional --NEW
+//Class Based component --OLD
+
+//Name of component starts with capital letter--Good practice
+
+//component composition --passing components as props to other components
+const HeaderComponent = () => {
+  return (
+    <div>
+    <Title/>
+      {/*  {Title()}  */}
+      <h2> Namaste React Functional component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
   );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "Heading1"
-  );
-  
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading1] 
-  );
-  
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(container);
+};
+
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(<HeaderComponent/>);
